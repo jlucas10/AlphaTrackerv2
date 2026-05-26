414c5bc1f521ef4485e9660ef62edaf366ab8ed5
Josiah Lucas <jolucas5335@gmail.com>
2026-05-26 10:37:30 -0700

    Initial commit: Spring Boot and React Vite skeleton

---
+++ src/main/java/com/example/alphatracker/AlphaTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.alphatracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlphaTrackerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AlphaTrackerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/alphatracker/AlphaTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.alphatracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AlphaTrackerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
